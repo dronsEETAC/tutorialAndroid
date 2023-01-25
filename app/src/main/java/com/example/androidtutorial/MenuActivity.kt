ce4f382f7e556e32bc86ddc1c821f44bd142f72e
lauranuez <lauranuezzaragoza@gmail.com>
2023-01-25 12:32:30 +0100

    Step 3: AlertDialog

--- app/src/main/java/com/example/androidtutorial/MenuActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MenuActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidtutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 
 class MenuActivity : AppCompatActivity() {
     private lateinit var showAlertButton: Button
@@ -17,12 +18,25 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         showAlertButton = findViewById(R.id.showAlert_btn)
+        showAlertButton.setOnClickListener { showAlert() }
         inputButton = findViewById(R.id.input_btn)
         getValueButton = findViewById(R.id.getValue_btn)
         parametersButton = findViewById(R.id.parameters_btn)
         videoButton = findViewById(R.id.video_btn)
         mapButton = findViewById(R.id.map_btn)
-
-
+    }
+    private fun showAlert(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage("Do you want to close this application?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { _, _ ->
+                finish()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.cancel()
+            }
+        val alert = dialogBuilder.create()
+        alert.setTitle("AlertDialog")
+        alert.show()
     }
 }
