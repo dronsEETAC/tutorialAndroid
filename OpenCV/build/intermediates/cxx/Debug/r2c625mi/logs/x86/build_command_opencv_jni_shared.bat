9b69ee881e45954e711ff9ceb439f2e0024c0dbd
lauranuez <lauranuezzaragoza@gmail.com>
2023-06-02 23:48:14 +0200

    Step12: OpenCV

---
+++ OpenCV/build/intermediates/cxx/Debug/r2c625mi/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Asus\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Asus\\Desktop\\TFG\\andoridTutorial\\OpenCV\\.cxx\\Debug\\r2c625mi\\x86" ^
+  opencv_jni_shared
--- app/src/main/java/com/example/androidtutorial/VideoActivity.kt
+++ app/src/main/java/com/example/androidtutorial/VideoActivity.kt
@@ -5,9 +5,15 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.MatOfByte
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
 
 class VideoActivity : AppCompatActivity(), ClientObserver {
     private lateinit var startButton: Button
@@ -17,6 +23,8 @@ class VideoActivity : AppCompatActivity(), ClientObserver {
     private lateinit var normalButton: Button
     private lateinit var imagenView: ImageView
     private lateinit var mqttClient: MqttClientClass
+    private var canny : Boolean = false
+    private  var gray : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +45,26 @@ class VideoActivity : AppCompatActivity(), ClientObserver {
 
         mqttClient = MqttClientClass.getMqttInstance(this)
         MqttClientClass.addObserver(this)
-        start()
+        if (OpenCVLoader.initDebug()){
+            start()
+        }
+
 
     }
 
     private fun normal() {
-
+        gray = false
+        canny = false
     }
 
     private fun gray() {
-
+        gray = true
+        canny = false
     }
 
     private fun canny() {
-
+        gray = false
+        canny = true
     }
 
     private fun stop() {
@@ -66,11 +80,27 @@ class VideoActivity : AppCompatActivity(), ClientObserver {
         if (mqttClient.newImage){
             runOnUiThread{
                 val decodedByte = Base64.decode(mqttClient.imageByteArray, Base64.DEFAULT)
-                val bitmap : Bitmap = BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size)
+                val bitmap : Bitmap = if(canny||gray){
+                    cannyOrGray(decodedByte)
+                }else{
+                    BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size)
+                }
                 imagenView.setImageBitmap(bitmap)
             }
             MqttClientClass.newImageChange(false)
         }
+    }
 
+    private fun cannyOrGray(byteArray: ByteArray): Bitmap {
+        val matOfByte = MatOfByte(*byteArray)
+        val mat = Imgcodecs.imdecode(matOfByte, Imgcodecs.IMREAD_UNCHANGED)
+        Imgproc.cvtColor(mat,mat,Imgproc.COLOR_RGBA2GRAY)
+        if (canny){
+            Imgproc.Canny(mat, mat, 80.0, 100.0)
+        }
+        val bitmap = Bitmap.createBitmap(mat.cols(),mat.rows(), Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(mat, bitmap)
+
+        return bitmap
     }
 }
